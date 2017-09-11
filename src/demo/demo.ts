6266fc46aacab8332d174ba2721b1e7f7233e9bf
朱仕杰 <zhusj@jiedaibao.com>
2017-09-11 14:14:43 +0800

    zhusj

---
+++ src/demo/demo.ts
@@ -0,0 +1,26 @@
+import { TestMoel } from './responseModel';
+export class Demo {
+    constructor() {
+        let test = new TestMoel();
+        test = new TestMoel();
+        test.$watch = (data) => {
+            /** 赋值触发的观察回调事件 data=>test当前 */
+          console.log(data);
+        };
+        /** 赋值触发以上的回调事件 */
+        test.array = [1, 2, 3];
+        test.name = "123";
+        test.age = 12;
+        test.inner = new TestMoel();
+        test.inner.name = "666";
+        test.inner.age = 888;
+        test.modelarray = [];
+        test.modelarray.push(new TestMoel());
+        test.modelarray.push(new TestMoel());
+        /** 根据注解器转成需要的JSON格式 */
+        let a = test.Serializable();
+        /** 根据JSON字符串 和类型 生成带有原型实体的对象 */
+        let obj = TestMoel.InstanceOf('{"name":"123","age":12,"inner":{"name":"666","age":888,"inner":null,"array":null,"modelarray":null},"array":[1,2,3],"modelarray":[{"name":null,"age":null,"inner":null,"array":null,"modelarray":null},{"name":null,"age":null,"inner":null,"array":null,"modelarray":null}]}', TestMoel);
+        debugger
+    }
+}
--- src/demo/responseModel.ts
+++ src/demo/responseModel.ts
@@ -1,59 +1,64 @@
 import { JsonProp, JsonSerializable, JsonModel, JsonModelArray, JsonPropArray } from './../_base/jsonModify';
 export class TestMoel extends JsonSerializable<TestMoel> {
-    
-        constructor() {
-            super();
-        }
-    
-        private _name: string;
-    
-        @JsonProp()
-        public get name(): string {
-            return this._name;
-        }
-        public set name(value: string) {
-            this._name = value;
-        }
-    
-        @JsonProp()
-        public get age(): number {
-            return this._age;
-        }
-    
-        public set age(value: number) {
-            this._age = value;
-        }
-    
-        private _age: number;
-    
-        @JsonModel(TestMoel)
-        public get inner(): TestMoel {
-            return this._inner;
-        }
-    
-        public set inner(value: TestMoel) {
-            this._inner = value;
-        }
-    
-        private _inner: TestMoel;
-    
-        @JsonPropArray()
-        public get array(): number[] {
-            return this._array;
-        }
-        public set array(value: number[]) {
-            this._array = value;
-        }
-    
-        private _array: number[];
-    
-        @JsonModelArray(TestMoel)
-        public get modelarray(): TestMoel[] {
-            return this._modelarray;
-        }
-    
-        public set modelarray(value: TestMoel[]) {
-            this._modelarray = value;
-        }
-        private _modelarray: TestMoel[];
+
+    constructor() {
+        super();
     }
+
+    private _name: string;
+    
+    /** 标识为一般值类型的属性用JsonProp */
+    @JsonProp()
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+
+    /** 标识为一般值类型的属性用JsonProp */
+    @JsonProp()
+    public get age(): number {
+        return this._age;
+    }
+
+    public set age(value: number) {
+        this._age = value;
+    }
+
+    private _age: number;
+
+    /** 标识为引用类型的属性用JsonModel PS: 该引用类型必须继承 JsonSerializable<T>  */
+    @JsonModel(TestMoel)
+    public get inner(): TestMoel {
+        return this._inner;
+    }
+
+    public set inner(value: TestMoel) {
+        this._inner = value;
+    }
+
+    private _inner: TestMoel;
+
+     /** 标识为值类型数组 一般为string[] number[] */
+    @JsonPropArray()
+    public get array(): number[] {
+        return this._array;
+    }
+    public set array(value: number[]) {
+        this._array = value;
+    }
+
+    private _array: number[];
+
+    /** 标识为引用类型数组 PS：目前只支持单一类型数组，且该类型同样需要继承 JsonSerializable<T>  */
+    @JsonModelArray(TestMoel)
+    public get modelarray(): TestMoel[] {
+        return this._modelarray;
+    }
+
+    public set modelarray(value: TestMoel[]) {
+        this._modelarray = value;
+    }
+    private _modelarray: TestMoel[];
+}
