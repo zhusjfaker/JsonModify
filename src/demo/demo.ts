85c4b0e1efdb35dc84e41463410ecbe6de7c3f8f
朱仕杰 <zhusj@jiedaibao.com>
2017-11-28 15:29:45 +0800

    zhusj

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,6 +1,10 @@
-import { JsonConvert } from './../_base/jsonModify';
+import { JsonConvert, JsonList } from './../_base/jsonModify';
 import { TestMoel } from './responseModel';
 export class Demo {
+
+    /** 新增数组特性 */
+    public list: JsonList<TestMoel>;
+
     constructor() {
         let test = new TestMoel();
         test = new TestMoel();
@@ -23,7 +27,18 @@ export class Demo {
         /** 根据注解器转成需要的JSON格式 */
         let a = test.Serializable();
         /** 根据JSON字符串 和类型 生成带有原型实体的对象 */
-        let obj = JsonConvert.InstanceOf<TestMoel>(TestMoel,'{"name":"123","age":12,"inner":{"name":"666","age":888,"inner":null,"array":null,"modelarray":null},"array":[1,2,3],"modelarray":[{"name":null,"age":null,"inner":null,"array":null,"modelarray":null},{"name":null,"age":null,"inner":null,"array":null,"modelarray":null}]}');
+        let obj = JsonConvert.InstanceOf<TestMoel>(TestMoel, '{"name":"123","age":12,"inner":{"name":"666","age":888,"inner":null,"array":null,"modelarray":null},"array":[1,2,3],"modelarray":[{"name":null,"age":null,"inner":null,"array":null,"modelarray":null},{"name":null,"age":null,"inner":null,"array":null,"modelarray":null}]}');
+
         debugger
+        /** 新增数组特性 */
+        this.list.push(test);
+        /** 批量序列化 */
+        let objlist = this.list.SerializableList();
+        let str = this.list.SerializableListStringify();
+        /** 数组批量还原 */
+        this.list = JsonConvert.ListOf<TestMoel>(TestMoel, str);
+
+        debugger
+
     }
 }
